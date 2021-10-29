0af2c44933ba32c8a45a7863237e6f1f03a1325d
Kostyukovich_V_A <demon4ik228228@gmail.com>
2021-10-29 23:35:14 +0300

    Implemented Test Classes

---
+++ TestClasses/BasicTest.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyCode
+{
+    class MyClass 
+    {
+        public void FirstMethodTest() 
+        {
+        }
+		
+		public void SecondMethodTest() 
+        {
+        }
+    }
+}
